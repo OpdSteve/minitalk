7815623a0b72ac3850b2d112f0bd1e1245cf3e56
Esteban Leon Gonzalez <eleon-go@c3r9s4.42malaga.com>
2023-08-22 15:07:48 +0200

    server added

--- server.c
+++ server.c
@@ -6,10 +6,40 @@
 /*   By: eleon-go@student.42malaga.co <eleon-go>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/03 16:41:36 by eleon-go@st       #+#    #+#             */
-/*   Updated: 2023/08/04 12:46:17 by eleon-go@st      ###   ########.fr       */
+/*   Updated: 2023/08/22 14:48:50 by eleon-go@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include "minitalk.h"
 
+void	ft_signaller(int signal)
+{
+	static char	character;
+	static int	bit;
+
+	if (signal == SIGUSR2)
+		character = character | (1 << (7 - bit));
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", (character));
+		bit = 0;
+		character = 0;
+	}
+}
+
+int	main(void)
+{
+	int	pid;
+
+	pid = getpid();
+	ft_printf("SERVER PID: %d\n", pid);
+	while (1)
+	{
+		signal(SIGUSR1, ft_signaller);
+		signal(SIGUSR2, ft_signaller);
+		pause();
+	}
+	return (0);
+}
