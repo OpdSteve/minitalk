73b6b2fe1c42798a9d430166f47c4968218b8ab6
OpdSteve <opdsteve@gmail.com>
2023-08-24 11:00:45 +0200

    Client bonus bug fixed

--- client_bonus.c
+++ client_bonus.c
@@ -6,14 +6,16 @@
 /*   By: eleon-go <eleon-go@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/23 13:11:45 by eleon-go@st       #+#    #+#             */
-/*   Updated: 2023/08/23 23:18:34 by eleon-go         ###   ########.fr       */
+/*   Updated: 2023/08/24 10:36:52 by eleon-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "./libft/libft.h"
 #include "minitalk.h"
 
-
+/*This is used for print the answer using signal, if signal is equality to
+*SIGUSR2 or SIGUSR1 print something that you wont (Is only for know if server
+*recived good the bits)*/
 static void	ft_signal_back(int signal)
 {
 	if (signal == SIGUSR2)
@@ -23,10 +25,10 @@ static void	ft_signal_back(int signal)
 }
 
 /*Use the PID proccess and a signal for send the string in form of bits.Then
-use SIGNUR2 to send 1 and 0, use Kill to send the bit for SIGNUR2. If is 
-diferent,send the signal in SIGNUR1 and wait 100 ms during the proccess to 
-not crash 1 letter of the string use 8 bits, when send 8 bits, use str++ to 
-continue for the string until the end*/
+*use SIGNUR2 to send 1 and 0, use Kill to send the bit for SIGNUR2. If is 
+*diferent,send the signal in SIGNUR1 and wait 100 ms during the proccess to 
+*not crash 1 letter of the string use 8 bits, when send 8 bits, use str++ to 
+*continue for the string until the end*/
 static void	ft_send_bit(int pid, char *str)
 {
 	int		bit;
@@ -48,6 +50,12 @@ static void	ft_send_bit(int pid, char *str)
 	}
 }
 
+/*This main use two argument, the first is the PID number proccess, this use
+*atoi to brake the PID and use it. The second argument is the text that we
+*would like to send, that is the second string. If you trie to use the client
+*with out the correct order, the program send a error mensage and close.
+*If all are OK, use the send_bit for send the bits to the server funtions
+*For other part, use signal with signal_back to send something to the server*/
 int	main(int argc, char **argv)
 {
 	int			pid;
